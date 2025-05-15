4c9ea95d51458b19702949a4f2466049adc0936a
Saqib Sohail <sohail.cpp@gmail.com>
2025-05-15 21:58:56 +0200

    add versel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Inter } from 'next/font/google';
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -98,6 +99,7 @@ export default function RootLayout({
       <body className="bg-gray-900 text-white antialiased">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
