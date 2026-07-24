45e029fecec581c2ed8226aa911f4b16dc3f5aca
Saqib <saqib@tacticaltech.org>
2026-07-24 14:02:12 +0200

    chore: upgrade Next.js to 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+import { fileURLToPath } from 'node:url';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  outputFileTracingRoot: fileURLToPath(new URL('.', import.meta.url)),
 
   // Performance optimizations
   experimental: {
